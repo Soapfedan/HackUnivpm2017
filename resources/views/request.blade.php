e627d22b20077dbd591a37b81d690414091fee7a
Federico Simonetti <federicosimonetti94@gmail.com>
2017-11-30 19:48:31 +0100

    dd

--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -1,5 +1,60 @@
 @extends('layouts.master')
 @section('content')
+<br><br><br><br>
+<br><br><br><br>
 
-Richieste
+<section>
+	<?= 'la spesa che devo fare'?>
+ @foreach ($orders as $order)
+		        
+		           @if($id==$order->id_buyer )
+		             <li> <?php
+		           	
+		          //  echo $order->id; 
+		          echo $order->id_buyer ;
+		            echo $order->id_customer; 
+		           echo $order->order_state ;
+		            echo $order->products_list; 
+		            echo $order->grand_total;
+		            ?></li>
+@endif @endforeach
+</section>
+<hr>
+<section>
+	<?= 'la spesa che devo ricevere'?>
+ @foreach ($orders as $order)
+		        
+		           @if($id==$order->id_customer )
+		           <li> <?php
+		
+		           // echo $order->id; 
+		          echo $order->id_buyer ;
+		            echo $order->id_customer; 
+		           echo $order->order_state ;
+		            echo $order->products_list; 
+		            echo $order->grand_total;
+		            ?></li>
+@endif @endforeach
+</section>
+<hr>
+<section>
+	<?= 'tutte le richieste'?>
+@foreach ($orders as $order)
+		        
+		           @if($id!=$order->id_customer && $id!=$order->id_buyer )
+		            	  <li> <?php
+		           	
+		           // echo $order->id; 
+		          echo $order->id_buyer ;
+		            echo $order->id_customer; 
+		           echo $order->order_state ;
+		            echo $order->products_list; 
+		            echo $order->grand_total;
+		            ?></li>
+@endif @endforeach
+</section>
+		      
+		          
+
+		    
 @endsection
