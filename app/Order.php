5da99b81bf353b6bee0dd5f63d3726857a3f5308
Simox <piccio.94@hotmail.it>
2017-11-30 17:35:57 +0100

    naan

---
+++ app/Order.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'id_buyer', 'id_shopper','order_state','products_list','grand_total','created_at','updated_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'remember_token'
+    ];
+}
