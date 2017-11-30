cc9ee5d775369c4e01f54788edfd2470e785f3df
Simox <piccio.94@hotmail.it>
2017-11-30 19:48:39 +0100

    naaan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,15 +23,15 @@
           </div>
           <div class="slide">
             <h2 class="slide__title">#save</h2>
-            <small>Risparmia tempo, non sottrarlo ai tuoi familiari, al tuo lavoro, alle tue passioni.
-          Risparmia denaro, i nostri prezzi saranno sempre i più competitivi sul mercato.
+            <small>Risparmia tempo, non sottrarlo ai tuoi familiari, al tuo lavoro, alle tue passioni.<br>
+          Risparmia denaro, i nostri prezzi saranno sempre i più competitivi sul mercato.<br>
           Risparmia inquinamento, ogni persona che condivide la propria spesa evita l’emissione di CO2.
           </small>
           </div>
           <div class="slide">
             <h2 class="slide__title">#stopwaste</h2>
-            <small>Non sprecare tempo, usane una parte per fare spesa anche per altri.
-          Guadagnerai due volte.
+            <small>Non sprecare tempo, usane una parte per fare spesa anche per altri.<br>
+          Guadagnerai due volte. 
           Fai risparmiare tempo a chi oggi non ne ha, domani potrebbe servire (a) te!
           </small>
           </div>
@@ -48,8 +48,33 @@
       
       <!-- COME FUNZIONA -->
       <section class="py-5">
-        <h3 class="text-center display-4">Come funziona</h3>
-        <p class="lead text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo arcu at finibus mollis. Morbi tempor orci sagittis quam rutrum sagittis. Vivamus sit amet molestie lectus. Aenean diam arcu, consectetur eu suscipit ultrices, posuere vitae nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent ac fringilla mauris, varius finibus ex.<br><br>
+        <h3 class="text-center display-4">Come funziona?</h3>
+        <p class="lead text-center">
+          Devi fare spesa?
+          Posta la tua lista della spesa, e aspetta solamente che qualcuno la prenda in carico.
+          <br>&#8595;<br>
+          Accetta lo Shopper
+          Vedi chi vuole fare la tua spesa. Guarda le sue recensioni, scegli se affidargli l’incarico o aspettare il prossimo, e se ti va poi valutalo. La Tua spesa per noi è importante, vogliamo che lo sia anche per chi se ne occuperà.
+          <br>&#8595;<br>
+          Aspetta.
+          Non ti resta che aspettare che la spesa arrivi all’orario concordato. Sarei libero di fare ciò che dovevi, senza alcuna preoccupazione.
+        <br><br>
+
+        <a href="#" class="btn btn-primary btn-lg">Scopri di più &#8594;</a></p>
+      </section>
+
+      <section class="py-5">
+        <h3 class="text-center display-4">Perché LetMeBuy.It?</h3>
+        <p class="lead text-center">
+        La vera domanda è: perché no?!
+        Ce lo siamo chiesti più volte prima di buttarci in questa avventura.
+        Ore interminabili di attesa alle casse, perdite di tempo, una vita di corsa, tutto questo per comprare magari 3 prodotti.
+
+        Ma perché farlo se qualcuno può e vuole farlo per Voi?
+
+        Let Me Buy It si propone di diventare la prima e più grande community al mondo di condivisione della spesa domestica: la piattaforma mette in contatto persone impossibilitate a sopperire ad un bisogno primario come quello della spesa domestica con chi invece sarebbe andato oggi stesso a far spesa per sé.
+
+        <br><br>
 
         <a href="#" class="btn btn-primary btn-lg">Scopri di più &#8594;</a></p>
       </section>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,14 +3,17 @@
     <div class="container">
       <div class="row py-5">
         <div class="col-md-5">
-          <h3><strong>LetMeBuy</strong></h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in imperdiet est. Suspendisse non feugiat ante. Nam magna odio, rhoncus ut iaculis condimentum, consectetur a odio.</p>
+          <h3><strong>Contattaci</strong></h3>
+          <p>Per te siamo sempre presenti.
+              Scrivici per qualsiasi problema, il nostro supporto tecnico ti risponderà e troveremo insieme una soluzione.
+
+          </p>
         </div>
 
         <div class="col-md-2"></div>
 
         <div class="col-md-5 text-right socials">
-          <p><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;&nbsp;info@letmebuy.it</p>
+          <p><i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp;&nbsp;letmebuy@customerservice.com</p>
           <i class="fa fa-facebook-square social-icon" aria-hidden="true"></i>
           <i class="fa fa-twitter-square social-icon" aria-hidden="true"></i>
           <i class="fa fa-instagram social-icon" aria-hidden="true"></i>
