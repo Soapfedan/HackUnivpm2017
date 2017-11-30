140785c54e864b1f15a4c4f7886e66d97e745f59
Biuni <info.gianlucabonifazi@gmail.com>
2017-11-30 17:45:33 +0100

    aa

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,264 @@
 @extends('layouts.master')
 @section('content')
 
-Dashboard
+<br><br><br><br>
+<main class="catalogs mt-4">
+<div class="container">
+    <h4 class="display-4">Catalogo</h4>
+    <div class="row">
+
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+    	<div class="col-md-2 py-2">
+			<div class="card">
+			  <img class="card-img-top" src="http://www.spesasimply.it/media/catalog/product/cache/2/small_image/185x/a4e40ebdc3e371adff845072e1c73f37/9/0/908102.jpg" alt="Card image cap">
+			  <div class="card-body">
+			    <h4 class="card-title">Glade Microspry</h4>
+			    <p class="card-text">15€</p>
+			    <a href="#" class="btn btn-success btn-sm">Acquista</a>
+			  </div>
+			</div>
+    	</div>
+
+    </div>
+</div>
+</main>
 @endsection
