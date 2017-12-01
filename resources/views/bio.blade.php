8482c18ec50262f0df1d70493f780ce9fed31939
Biuni <info.gianlucabonifazi@gmail.com>
2017-12-01 06:50:20 +0100

    fsdfsdfsd

--- resources/views/bio.blade.php
+++ resources/views/bio.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.master')
 
 @section('content')
-<br><br><br><br>
-<br><br><br><br>
-<br><br><br><br>
+<br><br><br><br><br><br>
 
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <?php
-            echo $user->name.' '.
-            $user->email.' '.
-            $user->surname.' '.
-            $user->address.' '.
-            $user->civic_number.' '.
-            $user->zip_code.' '.
-            $user->country.' '.
-            $user->telephone;
-            ?>
+        <div class="col-md-4">
+            <i class="fa fa-user-circle-o fa-3x" aria-hidden="true"></i>
+            <h3 style="display: inline-block;" class="align-middle"><strong>{{$user->name}} {{$user->surname}}</strong></h3>
+
+            <ul class="pl-5">
+                <li>{{$user->email}}</li>
+                <li>{{$user->address}}, {{$user->civic_number}} - {{$user->zip_code}}</li>
+                <li>{{$user->country}}</li>
+                <li>{{$user->telephone}}</li>
+            </ul>
+        </div>
+        <div class="col-md-8 text-right">
+            <h4>Voto degli utenti: <span class="badge badge-secondary">4.50</span></h4>
         </div>
     </div>
 </div>
