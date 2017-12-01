48666f694e43a774f58b0f963c50e66bc62e9c20
Biuni <info.gianlucabonifazi@gmail.com>
2017-12-01 01:26:18 +0100

    asd

--- resources/views/request.blade.php
+++ resources/views/request.blade.php
@@ -1,23 +1,88 @@
 @extends('layouts.master')
 @section('content')
 <br><br><br><br>
-<br><br><br><br>
+<br><br>
 
-<section>
-	la spesa che devo andare a comprare io 
-	<?php print_r($todo) ?>
+<section class="container">
+
+	<h3>Richieste prese in carico da te</h3>
+	<div class="row">
+	@foreach($todo as $to)
+			<div class="col-md-3">
+				<div class="card border-{{$to[1]}}">
+				  <div class="card-body">
+				    <blockquote class="card-blockquote">
+				      <p><strong>Lista della spesa #{{$to[0]}}</strong></p>
+				      <ul>
+						@foreach($to[3] as $prod)
+				      	<li>{{$prod}}</li>
+				      	@endforeach
+				      	<li>...</li>
+				      </ul>
+				      <footer class="text-left"><h4><span class="badge badge-success">{{$to[2]}}€</span></h4></footer>
+				      <footer class="text-right"><a href="#" class="btn btn-primary btn-sm">Vedi tutto</a></footer>
+				    </blockquote>
+				  </div>
+				</div>
+			</div>
+	@endforeach
+	</div>
+
 </section>
-<hr>
-<section>
-	tutte le richieste che ho fatto per ricevere la spesa
-	<?php print_r($toreceive) ?>
+<br><br>
+<section class="container">
+
+	<h3>Le mie richieste</h3>
+	<div class="row">
+	@foreach($toreceive as $torec)
+			<div class="col-md-3">
+				<div class="card border-{{$torec[1]}}">
+				  <div class="card-body">
+				    <blockquote class="card-blockquote">
+				      <p><strong>Lista della spesa #{{$torec[0]}}</strong></p>
+				      <ul>
+						@foreach($torec[3] as $prod)
+				      	<li>{{$prod}}</li>
+				      	@endforeach
+				      	<li>...</li>
+				      </ul>
+				      <footer class="text-left"><h4><span class="badge badge-success">{{$torec[2]}}€</span></h4></footer>
+				      <footer class="text-right"><a href="#" class="btn btn-primary btn-sm">Vedi tutto</a></footer>
+				    </blockquote>
+				  </div>
+				</div>
+			</div>
+	@endforeach
+	</div>
+
 </section>
-<hr>
-<section>
-	tutte le altre richieste
-	<?php print_r($all) ?>
+<br><br>
+<section class="container">
+
+	<h3>Tutte le richieste</h3>
+	<div class="row">
+	@foreach($all as $alls)
+			<div class="col-md-3">
+				<div class="card border-{{$alls[1]}}">
+				  <div class="card-body">
+				    <blockquote class="card-blockquote">
+				      <p><strong>Lista della spesa #{{$alls[0]}}</strong></p>
+				      <ul>
+						@foreach($alls[3] as $prod)
+				      	<li>{{$prod}}</li>
+				      	@endforeach
+				      	<li>...</li>
+				      </ul>
+				      <footer class="text-left"><h4><span class="badge badge-success">{{$alls[2]}}€</span></h4></footer>
+				      <footer class="text-right"><a href="#" class="btn btn-primary btn-sm">Vedi tutto</a></footer>
+				    </blockquote>
+				  </div>
+				</div>
+			</div>
+	@endforeach
+	</div>
+
 </section>
-		      
 		          
 
 		    
